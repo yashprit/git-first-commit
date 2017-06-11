aff93fa9649c734c3fb46b91e7251ee3bab37c2a
Yashprit Singh <yashprits@gmail.com>
2017-06-11 20:33:36 +0530

    undefined

--- index.js
+++ index.js
@@ -8,12 +8,14 @@ function gitCommit(message, cb) {
   var command = 'git add -A && git commit -m "' + message + '"';
 
   let { stdout, stderr, code } = exec(command, { silent: true });
+  console.log(stdout, stderr, code )
   if(stderr) {
     cb(err);
     return;
   } else {
     var intialPush = 'git push origin -u master';
     let { stdout, stderr, code } = exec(command, { silent: true });
+    console.log(stdout, stderr, code )
     if (stderr) {
       cb(err)
       return;
