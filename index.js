8047e184f45c17dd01b968202f3c4d85ae74a09c
Yashprit Singh <yashprits@gmail.com>
2017-06-11 20:36:05 +0530

    undefined

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function gitCommit(message, cb) {
   var command = 'git add -A && git commit -m "' + message + '"';
 
   let { stdout, stderr, code } = exec(command, { silent: true });
-  console.log(stdout, stderr, code )
+  //console.log(stdout, stderr, code )
   if(stderr) {
     cb(err);
     return;
